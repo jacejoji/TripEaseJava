714c87c76d0ed2de66bb6202eec5bcb567fca7e5
unknown <jacejoji@gmail.com>
2025-11-04 13:26:54 +0530

    Update

---
+++ src/com/ust/bean/ReservationBean.java
@@ -0,0 +1,83 @@
+package com.ust.bean;
+
+public class ReservationBean {
+	private String reservationID;
+    private String userID;
+    private String routeID;
+    private String bookingDate;
+    private String journeyDate;
+    private String vehicleID;
+    private String driverID;
+    private String bookingStatus;
+    private double totalFare;
+    private String boardingPoint;
+    private String dropPoint;
+    public String getReservationID() {
+		return reservationID;
+	}
+	public void setReservationID(String reservationID) {
+		this.reservationID = reservationID;
+	}
+	public String getUserID() {
+		return userID;
+	}
+	public void setUserID(String userID) {
+		this.userID = userID;
+	}
+	public String getRouteID() {
+		return routeID;
+	}
+	public void setRouteID(String routeID) {
+		this.routeID = routeID;
+	}
+	public String getBookingDate() {
+		return bookingDate;
+	}
+	public void setBookingDate(String bookingDate) {
+		this.bookingDate = bookingDate;
+	}
+	public String getJourneyDate() {
+		return journeyDate;
+	}
+	public void setJourneyDate(String journeyDate) {
+		this.journeyDate = journeyDate;
+	}
+	public String getVehicleID() {
+		return vehicleID;
+	}
+	public void setVehicleID(String vehicleID) {
+		this.vehicleID = vehicleID;
+	}
+	public String getDriverID() {
+		return driverID;
+	}
+	public void setDriverID(String driverID) {
+		this.driverID = driverID;
+	}
+	public String getBookingStatus() {
+		return bookingStatus;
+	}
+	public void setBookingStatus(String bookingStatus) {
+		this.bookingStatus = bookingStatus;
+	}
+	public double getTotalFare() {
+		return totalFare;
+	}
+	public void setTotalFare(double totalFare) {
+		this.totalFare = totalFare;
+	}
+	public String getBoardingPoint() {
+		return boardingPoint;
+	}
+	public void setBoardingPoint(String boardingPoint) {
+		this.boardingPoint = boardingPoint;
+	}
+	public String getDropPoint() {
+		return dropPoint;
+	}
+	public void setDropPoint(String dropPoint) {
+		this.dropPoint = dropPoint;
+	}
+	
+
+}
