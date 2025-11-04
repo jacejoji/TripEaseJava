f7941f9eee2401bc99c42c4bbab8714fee38697d
unknown <jacejoji@gmail.com>
2025-11-04 14:07:20 +0530

    Update

--- src/com/ust/dao/TripEaseDAO.java
+++ src/com/ust/dao/TripEaseDAO.java
@@ -7,8 +7,51 @@ import com.ust.bean.ReservationBean;
 import com.ust.bean.RouteBean;
 import com.ust.bean.VehicleBean;
 import com.ust.service.Administrator;
+import com.ust.service.Customer;
 
-public class AdminDAO implements Administrator {
+public class TripEaseDAO implements Administrator,Customer{
+
+	@Override
+	public ArrayList<VehicleBean> viewVehiclesByType(String vehicleType) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ArrayList<VehicleBean> viewVehicleBySeats(int noOfSeats) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ArrayList<RouteBean> viewAllRoutes() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String bookVehicle(ReservationBean reservationBean) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean cancelBooking(String userID, String reservationID) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public ReservationBean viewBookingDetails(String reservationID) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ReservationBean printBookingDetails(String reservationID) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 	@Override
 	public String addVehicle(VehicleBean vehicleBean) {
@@ -87,6 +130,5 @@ public class AdminDAO implements Administrator {
 		// TODO Auto-generated method stub
 		return null;
 	}
-	
 
 }
